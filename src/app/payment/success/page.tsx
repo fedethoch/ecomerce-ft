89b6729a1891452308438de6f3a3d61149f51595
FedeTH <134465117+fedethoch@users.noreply.github.com>
2025-09-18 13:37:51 -0300

    Update page.tsx

--- src/app/payment/success/page.tsx
+++ src/app/payment/success/page.tsx
@@ -1,27 +1,17 @@
 // SDK de Mercado Pago
-import { MercadoPagoConfig, Preference } from 'mercadopago';
-import 
+import { MercadoPagoConfig, Preference } from 'mercadopago'; 
 // Agrega credenciales
-const client = new MercadoPagoConfig({ accessToken: process.env.MP_ACCESS_TOKEN! });
+
 
 
 
 export function payment() {
-    const preference = new Preference(client)
-        .create({
-            items: {
-                id: "pago",
-                title: FormData.title,
-                description: FormData.description,
-                quantity: FormData.quantity,
-                currency_id: FormData.currency_id,
-                unit_price: FormData.unit_price
-            }
-        })
+    
 
 
     return (
-        <>
-        </>
+        <div>
+            <h1>Pago exitoso</h1>
+        </div>
     )
 }
